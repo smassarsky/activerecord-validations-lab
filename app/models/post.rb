145f2031959a58f847a27977f12f4aceb4384f36
smassarsky <seth.massarsky@gmail.com>
2020-12-01 12:53:39 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: { maximum: 250 }
+  validates :content, length: { minimum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :clickbait
+
+  def clickbait
+    if title && !title.match(/Won't Believe|Secret|Top \d+|Guess/)
+      errors.add(:clickbait, "not clickbaity enough")
+    end
+  end
+
 end
